92778cb7adefc14198644a953f80a4971e548379
Aaron <aarondeo30@gmail.com>
2025-03-22 15:59:22 -0700

    add dotween

---
+++ Assets/Scripts/SingletonMonoBehavior.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class SingletonMonoBehavior<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+    public static T Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<T>();
+                if (instance == null)
+                {
+                    GameObject obj = new GameObject(typeof(T).Name);
+                    instance = obj.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
