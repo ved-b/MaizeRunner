644e597ed2542fc0c667a0634e5fec4e76a93d1b
Juan Ablan <ablanjuan1@gmail.com>
2025-03-30 12:06:31 -0700

    Win Timer Created

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,17 +1,23 @@
+using TMPro;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] GameObject deathPanel;
-    [SerializeField] GameObject winPanel;
+    [SerializeField] private GameObject deathPanel;
+    [SerializeField] private GameObject winPanel;
+    [SerializeField] private TextMeshProUGUI finalTime;
+    [SerializeField] private GameObject inGameUI;
 
     public void ToggleDeathPanel()
     {
         deathPanel.SetActive(!deathPanel.activeSelf);
+        inGameUI.SetActive(false);
     }
 
     public void ToggleWinPanel()
     {
         winPanel.SetActive(!winPanel.activeSelf);
+        inGameUI.SetActive(false);
+        finalTime.text = "Final Time: " + inGameUI.GetComponentInChildren<TextMeshProUGUI>().text;
     }
 }
